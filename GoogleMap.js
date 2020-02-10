6712333f389314d4e9e3b1dd46905dcb3406a142
nvgunnell <60158507+nvgunnell@users.noreply.github.com>
2020-02-10 16:46:06 -0700

    Update GoogleMap.js

--- GoogleMap.js
+++ GoogleMap.js
@@ -4,10 +4,4 @@
           center: {lat: 40.295, lng: -111.7},
           zoom: 14
         });
-
-        var marker = new google.maps.Marker({
-    position: lat: 40.295776, lng: -111.711076,
-    title:"Mountain View High School"
-    });
-
       }
