f38a2f72473e45f978fa46cb655afa7d09a38be6
nvgunnell <nathan.gunnell@byu.edu>
2020-02-10 13:08:16 -0700

    Update GoogleMap.js

--- GoogleMap.js
+++ GoogleMap.js
@@ -1,7 +1,7 @@
       var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.397, lng: -111.644},
+          center: {lat: 40.30, lng: -111.7},
           zoom: 12
         });
       }
