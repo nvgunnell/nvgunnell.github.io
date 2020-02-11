3adb44730915e1b90fa89e3ad8df8f894a87dcee
nvgunnell <60158507+nvgunnell@users.noreply.github.com>
2020-02-11 14:49:39 -0700

    Update Manning.js

--- Manning.js
+++ Manning.js
@@ -5,7 +5,7 @@ var kinput = document.getElementById('kinput').value;
 var ninput = document.getElementById('ninput').value;
 var Rinput = document.getElementById('Rinput').value;
 var Sinput = document.getElementById('Sinput').value;
-var R = Math.pow (Rinput,0.66666666666666666666666666)
+var R = Math.pow (Rinput,2/3)
 
 var Result = kinput/ninput*R*Sinput;
 
