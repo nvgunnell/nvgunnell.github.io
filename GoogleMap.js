412dd814a96c7273c5a531a28f5a469bffe4af70
nvgunnell <60158507+nvgunnell@users.noreply.github.com>
2020-02-11 11:52:03 -0700

    New

--- GoogleMap.js
+++ GoogleMap.js
@@ -4,45 +4,209 @@
           center: {lat: 40.295, lng: -111.7},
           zoom: 14
         });
+
+                var contentMVHS = '<div id="content">'+
+               '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">Mountain View High School</h1>'+
+                '<div id="bodyContent">'+
+                  '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+
+                    var contentPM = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">Papa Murphys</h1>'+
+                '<div id="bodyContent">'+
+                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+
+                    var contentLJHS = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">Lakeridge Junior High School</h1>'+
+                '<div id="bodyContent">'+
+                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+
+                    var contentVin = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">Vineyard Elementary</h1>'+
+                '<div id="bodyContent">'+
+                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+
+                    var contentChurch = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">My Church</h1>'+
+                '<div id="bodyContent">'+
+                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+
+                    var contentHouse = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">My House</h1>'+
+                '<div id="bodyContent">'+
+                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+
       var MVHSlatlong = {lat: 40.295603, lng: -111.711145};
-      var marker = new google.maps.Marker({
+      var marker1 = new google.maps.Marker({
         position: MVHSlatlong,
         map: map,
         title:"Mountain View High School"
         });
+          var infowindow = new google.maps.InfoWindow({
+            content: contentMVHS,
+          });
+          marker1.addListener('click', function() {
+            infowindow.open(map, marker1);
+          });
 
       var PMlatlong = {lat: 40.297586, lng: -111.700476};
-      var marker = new google.maps.Marker({
+      var marker2 = new google.maps.Marker({
         position: PMlatlong,
         map: map,
         title:"Papa Murphy's"
         });
+          var infowindow = new google.maps.InfoWindow({
+            content: contentPM,
+          });
+          marker2.addListener('click', function() {
+            infowindow.open(map, marker2);
+          });
 
       var Vlatlong = {lat: 40.289223, lng: -111.737455};
-      var marker = new google.maps.Marker({
+      var marker3 = new google.maps.Marker({
         position: Vlatlong,
         map: map,
         title:"Vineyard Elementary"
         });
+          var infowindow = new google.maps.InfoWindow({
+            content: contentVin,
+          });
+          marker3.addListener('click', function() {
+            infowindow.open(map, marker3);
+          });
 
       var LJHSlatlong = {lat: 40.279940, lng: -111.704112};
-      var marker = new google.maps.Marker({
+      var marker4 = new google.maps.Marker({
         position: LJHSlatlong,
         map: map,
         title:"Lakeridge Jr. High"
         });
+          var infowindow = new google.maps.InfoWindow({
+            content: contentLJHS,
+          });
+          marker4.addListener('click', function() {
+            infowindow.open(map, marker4);
+          });
 
       var Churchlatlong = {lat: 40.285835, lng: -111.721751};
-      var marker = new google.maps.Marker({
+      var marker5 = new google.maps.Marker({
         position: Churchlatlong,
         map: map,
         title:"My Church"
         });
+          var infowindow = new google.maps.InfoWindow({
+            content: contentChurch,
+          });
+          marker5.addListener('click', function() {
+            infowindow.open(map, marker5);
+          });
+
 
       var Houselatlong = {lat: 40.289272, lng: -111.719670};
-      var marker = new google.maps.Marker({
+      var marker6 = new google.maps.Marker({
         position: Houselatlong,
         map: map,
         title:"My House"
         });
+          var infowindow = new google.maps.InfoWindow({
+            content: contentHouse,
+          });
+          marker6.addListener('click', function() {
+            infowindow.open(map, marker6);
+          });
       }
