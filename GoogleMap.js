24fcec881eea4a44b9ea61ec746f0086f6931f2a
nvgunnell <60158507+nvgunnell@users.noreply.github.com>
2020-02-10 17:32:39 -0700

    Update GoogleMap.js

--- GoogleMap.js
+++ GoogleMap.js
@@ -19,28 +19,28 @@
         });
 
       var Vlatlong = {lat: 40.289223, lng: -111.737455};
-      var marker1 = new google.maps.Marker({
+      var marker = new google.maps.Marker({
         position: Vlatlong,
         map: map,
         title:"Vineyard Elementary"
         });
 
       var LJHSlatlong = {lat: 40.279940, lng: -111.704112};
-      var marker1 = new google.maps.Marker({
+      var marker = new google.maps.Marker({
         position: LJHSlatlong,
         map: map,
         title:"Lakeridge Jr. High"
         });
 
       var Churchlatlong = {lat: 40.285835, lng: -111.721751};
-      var marker1 = new google.maps.Marker({
+      var marker = new google.maps.Marker({
         position: Churchlatlong,
         map: map,
         title:"My Church"
         });
 
       var Houselatlong = {lat: 40.289272, lng: -111.719670};
-      var marker1 = new google.maps.Marker({
+      var marker = new google.maps.Marker({
         position: Houselatlong,
         map: map,
         title:"My House"
