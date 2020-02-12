d4351f5fa46d02585fb94c0dbe2c0529191e9238
nvgunnell <60158507+nvgunnell@users.noreply.github.com>
2020-02-12 13:57:50 -0700

    New

--- Manning.js
+++ Manning.js
@@ -1,16 +1,36 @@
 
 function convertFunc(){
 
-var kinput = document.getElementById('kinput').value;
 var ninput = document.getElementById('ninput').value;
 var Rinput = document.getElementById('Rinput').value;
 var Sinput = document.getElementById('Sinput').value;
-var R = Math.pow (Rinput,2/3)
+var R = Math.pow (Rinput,2/3);
+var S = Math.pow (Sinput, 1/2);
+var ke = 1.486
+var ksi = 1.0
 
-var Result = kinput/ninput*R*Sinput;
+var Result;
 
-var x = document.createElement("INPUT");
-x.setAttribute("type", "radio");
+var english = document.getElementById("english");
+var si = document.getElementById("si");
 
-  document.getElementById('outResult').innerHTML = Result;
+
+
+    if (english.checked==true){
+    Result = ke/ninput*R*S + english.value;
+    }else if(si.checked==true){
+    Result = ksi/ninput*R*S + si.value;
+  }
+   else{
+        alert ("Unit type not selected!");
+        }
+
+    if (ninput <0.01 || ninput >0.06)
+        alert("n value not in range!")
+
+    if (ninput <0 || Rinput <0 || Sinput<0){
+        alert("Values cannot be negative!")
+        }else{
+        }
+  document.getElementById('outResult').innerHTML = 'Flow velocity = ' + Result;
 }
