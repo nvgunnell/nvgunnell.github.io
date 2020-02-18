6c740116a4e36da3d9085321c1e2498df353d910
nvgunnell <60158507+nvgunnell@users.noreply.github.com>
2020-02-18 14:43:24 -0700

    Photo

--- Leaflet.js
+++ Leaflet.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([40.295, -111.7], 14);
+var mymap = L.map('mapid').setView([40.295, -111.7], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -45,7 +45,7 @@ var hradius = L.circle([40.289251, -111.719683], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.4,
-    radius: 1600
+    radius: 1200,
 }).addTo(mymap);
 
 var hbound = L.polygon([
